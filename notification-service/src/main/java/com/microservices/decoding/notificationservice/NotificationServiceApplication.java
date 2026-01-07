67042acc16a131f58ae4ac2876d93ded89fbda12
serg10arg <cabrerasergio094@gmail.com>
2026-01-07 13:03:57 -0300

    feat(notification): implementar consumidor de eventos de pedidos
    Agrega dependencias de Spring Kafka y configura el listener
    para el tópico 'notificationTopic'.
    Define la deserialización de mensajes JSON a OrderPlacedEvent para
    procesar las notificaciones entrantes.
    
    --- Encabezado ---

--- notification-service/src/main/java/com/microservices/decoding/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/microservices/decoding/notificationservice/NotificationServiceApplication.java
@@ -1,4 +1,23 @@
 package com.microservices.decoding.notificationservice;
 
+import com.microservices.decoding.orderservice.event.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
 public class NotificationServiceApplication {
+
+    public static void main(String[] args)  {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Recibida notificacion para el pedido -{} ", orderPlacedEvent.getOrderNumber());
+    }
 }
